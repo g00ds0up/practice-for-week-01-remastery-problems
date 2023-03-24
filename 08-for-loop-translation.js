d9f26937f96d5bf16a75302a779284f49715bae2
g00ds0up <126825297+g00ds0up@users.noreply.github.com>
2023-03-24 00:18:04 -0400

    Update 08-for-loop-translation.js

--- 08-for-loop-translation.js
+++ 08-for-loop-translation.js
@@ -4,36 +4,9 @@ and returns the number of a's within that word. The function counts both
 lowercase (a) and uppercase (A). Your job is to translate the following function
 to use a `for` loop instead of the `while` loop it is currently using. If you
 forget the syntax for a `for` loop go back and check out the reading.
-*/
 
-function aCounter(word) {
-  let index = 0;
-  let count = 0;
-  while (index < word.length) {
-    let char = word[index];
-    if (char === "a" || char === "A") {
-      count += 1;
-    }
-    index++;
-  }
-  return count;
-};
+//your code goes here
 
-
-function aCounter(word) {
-    let index = 0;
-    let count = 0;
-
-    // change this while loop into a for loop
-    while (index < word.length) {
-        let char = word[index];
-        if (char === "a" || char === "A") {
-            count += 1;
-        }
-        index++;
-    }
-    return count;
-};
 
 console.log(aCounter("apple")); // => 1
 console.log(aCounter("appleapple")); // => 2
