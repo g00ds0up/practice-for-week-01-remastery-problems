06094b983bac1b5e7eaeb395296459584dea4d68
g00ds0up <126825297+g00ds0up@users.noreply.github.com>
2023-03-24 00:19:39 -0400

    Update 05-reverse-string.js

--- 05-reverse-string.js
+++ 05-reverse-string.js
@@ -6,16 +6,6 @@ should return a new string where the order of the characters is reversed.
 // Your code here
 
 
-let reverseString = function (str) {
-    let reverse = '';
-    for (let i = str.length - 1; i >= 0; i--) {
-        let word = str[i];
-        reverse += word;
-    }
-    return reverse
-};
-
-
 
 console.log(reverseString('fish')); // 'hsif'
 console.log(reverseString('marathon')); // 'nohtaram'
