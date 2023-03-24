1adecfdfdde1c208d2aafa2a4bd6cbe9008937dc
g00ds0up <126825297+g00ds0up@users.noreply.github.com>
2023-03-24 00:19:09 -0400

    Update 06-two-sum.js

--- 06-two-sum.js
+++ 06-two-sum.js
@@ -7,28 +7,6 @@ array contains only unique numbers.
 
 // Your code here
 
-
-let twoSum = function(arr, target) {
-// loop through arr
-for (let i = 0; i < arr.length; i++) {
-    // identify i
-    let num1 = arr[i];
-    // loop through arr again adding to i
-        for (let j = i + 1; j < arr.length; j++) {
-        let num2 = arr[j];
-        // if statement to find if array has chosen number
-        if(num1 + num2 === target) {
-            return true;
-        }
-         }
-    }
-    // if not true the return false
-    return false;
-};
-
-
-
-
 console.log(twoSum([1, 7, 3, 0, 2], 5)); // true
 console.log(twoSum([1, 7, 3, 0, 2], 6)); // false
 console.log(twoSum([4, 6, 2, 3], 8)); // true
