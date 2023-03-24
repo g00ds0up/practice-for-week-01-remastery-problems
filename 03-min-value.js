ef89b7f074538a08e6d8e29381f7cb1f6f5e01cc
g00ds0up <126825297+g00ds0up@users.noreply.github.com>
2023-03-24 00:20:28 -0400

    Update 03-min-value.js

--- 03-min-value.js
+++ 03-min-value.js
@@ -6,19 +6,6 @@ If the array is empty, the function should return `null`.
 */
 
 // Your code here
-let removeLastVowel = function(word) {
-    let vowel = 'aeiou' 
-    for (i = word.length - 1; i >= 0; i--) {
-        let letter = word[i];
-        if (vowel.includes(letter)) {
-            return word.slice(0, i) + word.slice (i + 1)
-        }
-    } return word
-};
-
-
-
-
 
 
 console.log(minValue([4, 6, 3, 5, 2, 4])); // 2
